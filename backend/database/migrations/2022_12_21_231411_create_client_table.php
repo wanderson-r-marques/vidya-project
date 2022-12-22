64c3cd73ffe9a31c0d154fec8122527c29eb648e
Wanderson R Marques <wrm@cin.ufpe.br>
2022-12-22 09:21:36 -0300

    feat(backend): add customer and product migrations
    The migrations were created using the many-to-many relationship

---
+++ backend/database/migrations/2022_12_21_231411_create_client_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('cnpj',14)->unique();
+            $table->string('corporate_name');
+            $table->string('fantasy _name');
+            $table->string('zipcode',9);
+            $table->string('adreess');
+            $table->integer('number')->nullable();
+            $table->string('complement')->nullable();
+            $table->string('district');
+            $table->string('city');
+            $table->string('state');
+            $table->string('email');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client');
+    }
+};
---
+++ backend/database/migrations/2022_12_22_013358_create_client_product_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_product', function (Blueprint $table) {
+            $table->foreignId('client_id')->constrained();
+            $table->foreignId('product_id')->constrained();
+            $table->date('expiration_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_product');
+    }
+};

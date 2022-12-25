c05cecfb810475e71f47a6a4e5e80b24b21108a7
Wanderson R Marques <wrm@cin.ufpe.br>
2022-12-25 09:31:04 -0300

    feat(backend): create customer and product routes

--- backend/app/Http/Controllers/ClientController.php
+++ backend/app/Http/Controllers/ClientController.php
@@ -2,49 +2,62 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Client;
+use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        $clients = Client::all();
+        $clients = Client::paginate(10);
         return $clients;
     }
 
-
-    public function create()
-    {
-        //
-    }
-
-
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         //
     }
 
-
-    public function show($id)
+    /**
+     * Display the specified resource.
+     *
+     * @param  Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Client $client)
     {
-        //
+        return $client;
     }
 
-
-    public function edit($id)
-    {
-        //
-    }
-
-
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, $id)
     {
         //
     }
 
-
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
         //
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,4 +9,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+Route::apiResource('clients', ClientController::class);
